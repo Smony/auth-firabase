b34bdca5ebc546cde3404e59e83379772ea79f7a
Vladymyr Symonchuk <dev.symonchuk@gmail.com>
2020-09-24 18:12:59 +0300

    add bottom Navigation Bar

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -2,12 +2,21 @@ import 'package:banking/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:banking/models/bank.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: new Scaffold(
         backgroundColor: Theme.of(context).primaryColor,
+
         appBar: AppBar(
           title: Text(
             'BankingApp',
@@ -26,7 +35,33 @@ class HomePage extends StatelessWidget {
             ),
           ],
         ),
+
         body: BankList(),
+
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              title: Text('Business'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              title: Text('School'),
+            ),
+          ],
+          currentIndex: 0,
+          selectedItemColor: Theme.of(context).primaryColor,
+          onTap: (int index){
+            setState(() {
+              selectIndex = index;
+            });
+          },
+        ),
+
       ),
     );
   }
