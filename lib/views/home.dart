b302fe7a0f03caddf16718ec635e0b4eeea5ccdb
Vladymyr Symonchuk <dev.symonchuk@gmail.com>
2020-09-24 22:02:05 +0300

    custom bottom bar

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:banking/services/auth.dart';
+import 'package:ff_navigation_bar/ff_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:banking/models/bank.dart';
 
@@ -36,30 +37,32 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
 
-        body: BankList(),
+        body: selectIndex == 0 ? BankList() : PersonalList(),
 
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              title: Text('Home'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              title: Text('Business'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.school),
-              title: Text('School'),
-            ),
-          ],
-          currentIndex: 0,
-          selectedItemColor: Theme.of(context).primaryColor,
-          onTap: (int index){
+        bottomNavigationBar: FFNavigationBar(
+          theme: FFNavigationBarTheme(
+            barBackgroundColor: Colors.white,
+            selectedItemBorderColor: Colors.black12,
+            selectedItemBackgroundColor: Theme.of(context).primaryColor,
+            selectedItemIconColor: Colors.white,
+            selectedItemLabelColor: Colors.black,
+          ),
+          selectedIndex: selectIndex,
+          onSelectTab: (index) {
             setState(() {
               selectIndex = index;
             });
           },
+          items: [
+            FFNavigationBarItem(
+              iconData: Icons.home,
+              label: 'Schedule',
+            ),
+            FFNavigationBarItem(
+              iconData: Icons.person_outline,
+              label: 'Contacts',
+            ),
+          ],
         ),
 
       ),
@@ -89,6 +92,39 @@ class BankList extends StatelessWidget {
               child: ListTile(
                 title: Text(
                   bank[index].title,
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class PersonalList extends StatelessWidget {
+  final bank = <Bank>[
+    Bank(title: 'fsdfds1', desc: 'dsfsdfds1', author: 'Rimma'),
+    Bank(title: 'fsdfds2', desc: 'dsfsdfds2', author: 'Smony'),
+    Bank(title: 'fsdfds3', desc: 'dsfsdfds3', author: 'Rimma'),
+    Bank(title: 'fsdfds4', desc: 'dsfsdfds4', author: 'Smony'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.builder(
+        itemCount: bank.length,
+        itemBuilder: (context, index) {
+          return Card(
+            elevation: 2.0,
+            margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+            child: Container(
+              decoration: BoxDecoration(color: Colors.amber),
+              child: ListTile(
+                title: Text(
+                  bank[index].author,
                   style: TextStyle(color: Colors.white, fontSize: 30),
                 ),
               ),
